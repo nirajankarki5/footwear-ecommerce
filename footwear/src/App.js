72819bd1636b79d2ae82f348c25788ec386c30e1
nirajankarki5 <nirajankarki5@gmail.com>
2023-10-26 17:32:41 -0400

    first commit

---
+++ footwear/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>HOME PAGE</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening at port 5000");
+});
