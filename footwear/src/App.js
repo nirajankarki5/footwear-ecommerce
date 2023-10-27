ac58ed20b219125b924c15cf0c0a253726e35c73
nirajankarki5 <nirajankarki5@gmail.com>
2023-10-27 19:33:36 -0400

    added frontend folder str

--- footwear/src/App.js
+++ footwear/src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "./pages/AppLayout";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import SingleProduct from "./pages/SingleProduct";
+import Login from "./pages/Login";
+import UserDetails from "./pages/UserDetails";
+import Error from "./pages/Error";
+import Cart from "./pages/Cart";
+
 function App() {
   return (
-    <div className="App">
-      <h1>HOME PAGE</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<SingleProduct />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="user" element={<UserDetails />} />
+
+          <Route path="*" element={<Error />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
