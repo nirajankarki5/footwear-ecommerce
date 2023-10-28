eaed86706cd3ec14460f376518b2489800291abb
nirajankarki5 <nirajankarki5@gmail.com>
2023-10-28 11:59:54 -0400

    tailwind initial setup

--- footwear/src/pages/Home.js
+++ footwear/src/pages/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Home() {
-  return <div>Home Component</div>;
+  return (
+    <div className="inline-block border bg-yellow-100 font-bold text-green-500">
+      Home Component
+    </div>
+  );
 }
 
 export default Home;
