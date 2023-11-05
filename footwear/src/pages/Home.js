d55fa5cc14a45df6ba174f5b1c0d7154aa435e70
nirajankarki5 <nirajankarki5@gmail.com>
2023-11-05 13:09:49 -0500

    homepage header

--- footwear/src/pages/Home.js
+++ footwear/src/pages/Home.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="font-body inline-block border bg-yellow-100 p-5 font-bold text-green-500">
-      Home Component
+    <div className="font-body ">
+      <section className="my-10 text-center md:my-12 lg:my-14">
+        <h1 className=" text-3xl font-medium md:text-4xl lg:text-5xl">
+          Shop Your Favourite Brand
+        </h1>
+        <p className="my-3 leading-6 text-gray-400 md:my-4 md:text-lg lg:my-5 lg:text-xl">
+          We have a bunch of collection for you!, Let's go explore and find your
+          dream shoes.
+        </p>
+      </section>
     </div>
   );
 }
