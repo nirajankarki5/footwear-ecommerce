be3e822d28326e8ad96d5a288c5bfc4e74ab36e7
nirajankarki5 <nirajankarki5@gmail.com>
2023-11-08 11:15:11 -0500

    added search by text api

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,7 +1,14 @@
 const Product = require("../models/Product");
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({});
+  const { searchTerm } = req.query;
+  const queryObject = {};
+
+  if (searchTerm) {
+    queryObject.name = { $regex: searchTerm, $options: "i" };
+  }
+
+  const products = await Product.find(queryObject);
   res.status(200).json(products);
 };
 
