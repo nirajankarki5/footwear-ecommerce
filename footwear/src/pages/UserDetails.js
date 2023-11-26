e06ac79aa9887c245f770347d3be84182ff88650
nirajankarki5 <nirajankarki5@gmail.com>
2023-11-26 14:39:44 -0500

    user details page responsive layout

--- footwear/src/pages/UserDetails.js
+++ footwear/src/pages/UserDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { CiUser } from "react-icons/ci";
 
 function UserDetails() {
   const navigate = useNavigate();
@@ -11,9 +12,18 @@ function UserDetails() {
   };
 
   return (
-    <div>
-      <p>User Details</p>
-      <button onClick={logout}>Logout</button>
+    <div className="grid h-full grid-rows-[1fr_1px_3fr] gap-8 px-5 py-4 md:grid-cols-[1fr_1px_3fr] md:grid-rows-none lg:px-10">
+      {/* <p>User Details</p>
+      <button onClick={logout}>Logout</button> */}
+      <section className="mx-auto mt-5 flex h-40 w-40 items-center justify-center rounded-full bg-gray-300 lg:h-52 lg:w-52">
+        <CiUser className="text-9xl lg:text-[150px]" />
+      </section>
+      <div className="border-2"></div>
+      <section className=" md:my-5">
+        <h1 className="text-xl font-semibold md:text-2xl lg:text-3xl">
+          My Details
+        </h1>
+      </section>
     </div>
   );
 }
