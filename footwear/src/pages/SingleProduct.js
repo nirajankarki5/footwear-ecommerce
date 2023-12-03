0dd0493163be04c0c97d4a0d8a54813457ffca71
nirajankarki5 <nirajankarki5@gmail.com>
2023-12-03 11:56:00 -0500

    singleproduct page error handling

--- footwear/src/pages/SingleProduct.js
+++ footwear/src/pages/SingleProduct.js
@@ -20,68 +20,68 @@ function SingleProduct() {
     dispatch(fetchSingleProduct("/" + id));
   }, [dispatch, id]);
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!isLoading && !singleProduct.name) {
+    return (
+      <p className="my-20 text-center text-2xl text-gray-400 md:my-32">
+        Sorry we can't find the product
+      </p>
+    );
+  }
   return (
-    <>
-      {isLoading && <Loading />}
-      {!isLoading && (
-        <div className="grid grid-rows-[auto_1fr] gap-5 px-5 py-4 md:grid-cols-2 md:grid-rows-none lg:px-10 xl:gap-10">
-          <section className="mx-auto flex h-96 w-full items-center justify-center self-start rounded-lg bg-stone-200 md:my-2 md:h-[70vh] lg:rounded-3xl">
-            <CiImageOn className="mx-auto text-[10rem]" />
-          </section>
-          <section className="p-3 lg:p-8">
-            <h1 className="mb-2 text-2xl font-semibold md:mb-3 md:text-3xl lg:text-4xl">
-              {singleProduct.name}
-            </h1>
+    <div className="grid grid-rows-[auto_1fr] gap-5 px-5 py-4 md:grid-cols-2 md:grid-rows-none lg:px-10 xl:gap-10">
+      <section className="mx-auto flex h-96 w-full items-center justify-center self-start rounded-lg bg-stone-200 md:my-2 md:h-[70vh] lg:rounded-3xl">
+        <CiImageOn className="mx-auto text-[10rem]" />
+      </section>
+      <section className="p-3 lg:p-8">
+        <h1 className="mb-2 text-2xl font-semibold md:mb-3 md:text-3xl lg:text-4xl">
+          {singleProduct.name}
+        </h1>
 
-            <div className="mb-5 flex items-center gap-2 md:mb-8">
-              <FaStar className="text-xl text-yellow-400" />
-              <p className="text-xl text-gray-300">
-                {singleProduct.rating} rating
-              </p>
-            </div>
-
-            <h1 className="mb-4 text-4xl font-semibold md:mb-5 md:text-4xl lg:text-5xl">
-              ${singleProduct.price}
-            </h1>
-
-            <p className="mb-5 text-xl md:mb-8 md:text-2xl">
-              Brand: {singleProduct.brand}
-            </p>
-
-            <p className="mb-1 text-gray-400 md:mb-3 md:text-xl">
-              Select Sizes:
-            </p>
-            <div className="mb-10 flex flex-wrap gap-2 md:mb-16">
-              {singleProduct.sizes.map((item) => {
-                return (
-                  <div
-                    key={item}
-                    onClick={() => setSize(item)}
-                    className={`${
-                      size === item
-                        ? "bg-gray-900 text-gray-200"
-                        : "text-gray-900"
-                    } flex h-20 w-20 cursor-pointer items-center justify-center border-2 border-gray-400 text-lg md:text-xl`}
-                  >
-                    {item}
-                  </div>
-                );
-              })}
-            </div>
-
-            <button className="mb-3 flex items-center gap-4 rounded-2xl bg-gray-900 px-10 py-5 text-xl text-gray-200 md:mb-5 lg:px-20">
-              <HiOutlineShoppingBag className="text-2xl" />
-              <p>Add to cart</p>
-            </button>
-
-            <div className="flex items-center gap-2 font-medium md:text-lg md:tracking-wide">
-              <LuTruck className="text-xl" />
-              <p>Free delivery on orders over $30.0</p>
-            </div>
-          </section>
+        <div className="mb-5 flex items-center gap-2 md:mb-8">
+          <FaStar className="text-xl text-yellow-400" />
+          <p className="text-xl text-gray-300">{singleProduct.rating} rating</p>
         </div>
-      )}
-    </>
+
+        <h1 className="mb-4 text-4xl font-semibold md:mb-5 md:text-4xl lg:text-5xl">
+          ${singleProduct.price}
+        </h1>
+
+        <p className="mb-5 text-xl md:mb-8 md:text-2xl">
+          Brand: {singleProduct.brand}
+        </p>
+
+        <p className="mb-1 text-gray-400 md:mb-3 md:text-xl">Select Sizes:</p>
+        <div className="mb-10 flex flex-wrap gap-2 md:mb-16">
+          {singleProduct.sizes?.map((item) => {
+            return (
+              <div
+                key={item}
+                onClick={() => setSize(item)}
+                className={`${
+                  size === item ? "bg-gray-900 text-gray-200" : "text-gray-900"
+                } flex h-20 w-20 cursor-pointer items-center justify-center border-2 border-gray-400 text-lg md:text-xl`}
+              >
+                {item}
+              </div>
+            );
+          })}
+        </div>
+
+        <button className="mb-3 flex items-center gap-4 rounded-2xl bg-gray-900 px-10 py-5 text-xl text-gray-200 md:mb-5 lg:px-20">
+          <HiOutlineShoppingBag className="text-2xl" />
+          <p>Add to cart</p>
+        </button>
+
+        <div className="flex items-center gap-2 font-medium md:text-lg md:tracking-wide">
+          <LuTruck className="text-xl" />
+          <p>Free delivery on orders over $30.0</p>
+        </div>
+      </section>
+    </div>
   );
 }
 
