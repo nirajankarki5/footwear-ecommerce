c478b0a6fc90e08c1efcd6ad3d9a1525a20edf50
nirajankarki5 <nirajankarki5@gmail.com>
2023-12-17 11:51:14 -0500

    added image in product card

--- footwear/src/components/ProductCard.js
+++ footwear/src/components/ProductCard.js
@@ -2,13 +2,15 @@ import React from "react";
 import { AiFillStar } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
-function ProductCard({ _id: id, name, price, desc, rating, brand }) {
+function ProductCard({ _id: id, name, price, desc, rating, brand, image }) {
   return (
     <Link
       to={`/products/${id}`}
       className="overflow-hidden rounded-xl p-3 shadow-md"
     >
-      <div className="h-60 rounded-xl bg-stone-100"></div>
+      <div className="flex h-60 items-center justify-center rounded-xl">
+        <img className="h-4/5 w-4/5 object-contain" src={image} alt={name} />
+      </div>
       <h2 className="my-1 mt-3 line-clamp-1 w-60 text-ellipsis text-lg font-medium">
         {name}
       </h2>
