4c9e32c5b05f74a2080c7d233c83faa5e528416c
nirajankarki5 <nirajankarki5@gmail.com>
2024-01-04 15:15:00 -0500

    nimor design changes

--- footwear/src/components/Footer.js
+++ footwear/src/components/Footer.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="font-body grid grid-rows-2 justify-between bg-stone-900 p-6 py-8 text-gray-300 lg:grid-cols-2 lg:grid-rows-1 lg:gap-8 lg:p-10 lg:py-12 xl:py-16">
+    <footer className="grid grid-rows-2 justify-between bg-stone-900 p-6 py-8 font-body text-gray-300 lg:grid-cols-2 lg:grid-rows-1 lg:gap-8 lg:p-10 lg:py-12 xl:py-16">
       <section className="text-sm">
         <h1 className="text-2xl font-semibold lg:text-3xl">FootWear</h1>
-        <p className="my-5 md:text-base lg:text-xl">
+        <p className="my-5 md:text-base lg:text-lg">
           Footwear was designed and founded in 2023. It's main goal is to
           provide all sorts of products to individuals looking to get a new pair
           of sneakers.
@@ -14,7 +14,7 @@ function Footer() {
       <section className="mt-2 grid grid-cols-3 justify-between lg:text-lg">
         <div>
           <h1 className="mb-4 text-xl font-medium lg:text-2xl">Products</h1>
-          <div className="flex flex-col gap-2 text-sm text-gray-300 md:text-base lg:gap-4 lg:text-xl">
+          <div className="flex flex-col gap-2 text-sm text-gray-300 md:text-base lg:gap-4 lg:text-lg">
             <p>All Products</p>
             <p>Shoes</p>
             <p>Apparel</p>
@@ -22,7 +22,7 @@ function Footer() {
         </div>
         <div>
           <h1 className="mb-4 text-xl font-medium lg:text-2xl">Collections</h1>
-          <div className=" flex flex-col gap-2 text-sm text-gray-300 md:text-base lg:gap-4 lg:text-xl">
+          <div className=" flex flex-col gap-2 text-sm text-gray-300 md:text-base lg:gap-4 lg:text-lg">
             <p>Nike</p>
             <p>Under Armour</p>
             <p>New Balance</p>
@@ -30,7 +30,7 @@ function Footer() {
         </div>
         <div>
           <h1 className="mb-4 text-xl font-medium lg:text-2xl">Support</h1>
-          <div className="flex flex-col gap-2 text-sm text-gray-300 md:text-base lg:gap-4 lg:text-xl">
+          <div className="flex flex-col gap-2 text-sm text-gray-300 md:text-base lg:gap-4 lg:text-lg">
             <p>Contact us</p>
             <p>Give feedback</p>
             <p>Help center</p>
