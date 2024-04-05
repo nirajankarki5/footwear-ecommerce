8cecadf977159a9585dfa50f75e09c6af9ed4d2a
nirajankarki5 <nirajankarki5@gmail.com>
2024-04-05 15:22:20 -0400

    admin dashboard usertype check

--- footwear/src/pages/AdminDashboard.js
+++ footwear/src/pages/AdminDashboard.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Loading from "../components/Loading";
+import { fetchUser } from "../features/user/userSlice";
 
 function AdminDashboard() {
-  return <h1>AdminDashboard</h1>;
+  const { isLoading, user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    document.title = "Dashboard";
+    const tokenString = localStorage.getItem("token");
+    if (tokenString) {
+      dispatch(fetchUser(JSON.parse(tokenString)));
+    }
+  }, [dispatch]);
+
+  if (user.userType === "User") {
+    return <h1>Please login as Admin to view this page</h1>;
+  }
+
+  return (
+    <>
+      {isLoading && <Loading />}
+      <h1>{user.email}</h1>
+    </>
+  );
 }
 
 export default AdminDashboard;
