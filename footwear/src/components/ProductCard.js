ddbe074379c65111a1ae8d90a9888a9d6d76c3ed
nirajankarki5 <nirajankarki5@gmail.com>
2024-04-09 22:14:14 -0400

    minor adjustments

--- footwear/src/components/ProductCard.js
+++ footwear/src/components/ProductCard.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { AiFillStar, AiOutlineDelete } from "react-icons/ai";
 import { FaRegEdit } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteProduct } from "../features/product/productSlice";
-import { useSearchParams } from "react-router-dom";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -23,6 +22,11 @@ function ProductCard({
   const dispatch = useDispatch();
   const [searchParams, setSearchParams] = useSearchParams();
 
+  // Show edit button to modal popup in page other page except homepage
+  // Admin can only edit product in Search Screen or Our Product page
+  const location = useLocation(); // React Hook
+  console.log(location.pathname);
+
   //   Toast notification
   const notifyDeleted = () => toast.success("Product has been deleted");
 
@@ -34,23 +38,26 @@ function ProductCard({
   return (
     <>
       <div className="overflow-hidden rounded-xl p-3 shadow-md">
-        <div className="flex h-60 items-center justify-center rounded-xl">
-          <img className="h-4/5 w-4/5 object-contain" src={image} alt={name} />
-        </div>
-        <Link
-          to={`/products/${id}`}
-          className="my-1 mt-3 line-clamp-1 w-60 text-ellipsis text-lg font-medium hover:underline-offset-1"
-        >
-          {name}
+        <Link to={`/products/${id}`}>
+          <div className="flex h-60 items-center justify-center rounded-xl">
+            <img
+              className="h-4/5 w-4/5 object-contain"
+              src={image}
+              alt={name}
+            />
+          </div>
+          <p className="my-1 mt-3 line-clamp-1 w-60 text-ellipsis text-lg font-medium hover:underline-offset-1">
+            {name}
+          </p>
+          <div className="flex gap-2">
+            <AiFillStar className="text-xl text-yellow-400" />
+            <p className="font-medium">{rating}</p>
+          </div>
         </Link>
-        <div className="flex gap-2">
-          <AiFillStar className="text-xl text-yellow-400" />
-          <p className="font-medium">{rating}</p>
-        </div>
 
         <div className="flex items-center justify-between">
           <p className="my-1 text-xl font-medium">${price}</p>
-          {user.userType === "Admin" && (
+          {user.userType === "Admin" && location.pathname !== "/" && (
             <div className="flex items-center justify-between gap-2">
               <AiOutlineDelete
                 className="cursor-pointer text-3xl text-red-500"
